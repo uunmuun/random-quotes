0ecdd0dc2aa064f117b89598ff82df1c2d40ad01
Parvani Vafa <porvanivafoo@gmail.com>
2025-08-05 22:56:53 +0600

    splitting app into the JS modules

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-const quotes = [
-  {
-    quote: "The only way to do great work is to love what you do",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Innovation distinguishes between a leader and a follower",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Your time is limited, so don't waste it living someone else's life",
-    author: "Steve Jobs",
-  },
-  {
-    quote:
-      "Success is not final, failure is not fatal: It is the courage to continue that counts",
-    author: "Winston Churchill",
-  },
-  {
-    quote: "Life is what happens when you're busy making other plans",
-    author: "John Lennon",
-  },
-  {
-    quote:
-      "Do not go where the path may lead, go instead where there is no path and leave a trail",
-    author: "Ralph Waldo Emerson",
-  },
-  {
-    quote: "Believe you can and you're halfway there",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote: "I have not failed. I've just found 10,000 ways that won't work",
-    author: "Thomas Edison",
-  },
-  {
-    quote:
-      "What lies behind us and what lies before us are tiny matters compared to what lies within us",
-    author: "Ralph Waldo Emerson",
-  },
-  {
-    quote: "The best way to predict the future is to invent it",
-    author: "Alan Kay",
-  },
-];
+import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
