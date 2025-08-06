364164440cf77f4860a2ebe12266e1bb6e6e5b19
Parvani Vafa <porvanivafoo@gmail.com>
2025-08-07 00:33:49 +0600

    changed fav icon to star

--- script.js
+++ script.js
@@ -3,17 +3,47 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleFavoriteBtn = document.getElementById("toggle-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  const quote = randomQuote.quote;
-  const quoteAuthor = randomQuote.author;
-  //   const quote = `<em>" ${randomQuote.quote} "</em><br> <span id="author"> - ${randomQuote.author} </span>`;
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
+  const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
-  quoteAuthorElement.textContent = quoteAuthor;
+  quoteAuthorElement.textContent = author;
+  toggleFavoriteBtn.classList.toggle("fa", randomQuote.isFavorite);
+  toggleFavoriteBtn.classList.toggle("far", !randomQuote.isFavorite);
+
+  toggleFavoriteBtn.style.display = "inline-block";
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteIcon(currentQuote.isFavorite);
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `
+        <p>${currentQuote.quote}</p>
+        <p class='author'>${currentQuote.author}</p>
+        `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
 
 // generateRandomQuote();
