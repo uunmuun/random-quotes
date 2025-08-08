c2df07e5b7823fc142e875d9ff3cf80cbb816861
Parvani Vafa <porvanivafoo@gmail.com>
2025-08-09 01:23:09 +0600

    Part of the favorites module refactoring - modify and rename btn

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,32 +1,31 @@
 import { currentQuote } from "../../index.js";
 
 const favoritesContainer = document.getElementById("favorites-container");
-const toggleBtn = document.getElementById("toggle-favorite-btn");
-toggleBtn.addEventListener("click", toggleFavorite);
+const favoriteBtn = document.getElementById("favorite-btn");
+favoriteBtn.addEventListener("click", () =>
+  toggleFavorite(currentQuote, favoriteBtn, favoritesContainer)
+);
 
-hideBtn(toggleBtn);
+hideBtn(favoriteBtn);
 
-function toggleFavorite() {
-  currentQuote.isFavorite = !currentQuote.isFavorite;
-  toggleFavoriteIcon(currentQuote.isFavorite, toggleBtn);
+function toggleFavorite(quote, btn, container) {
+  quote.isFavorite = !quote.isFavorite;
+  const { text, author, isFavorite } = quote;
+  toggleFavoriteBtnIcon(isFavorite, btn);
 
-  if (currentQuote.isFavorite) {
-    showFavoriteCard(
-      currentQuote.text,
-      currentQuote.author,
-      favoritesContainer
-    );
+  if (isFavorite) {
+    showFavoriteCard(text, author, container);
   } else {
-    hideFavoriteCard(currentQuote.text);
+    hideFavoriteCard(text);
   }
 }
 
 function handleFavorite(isFavorite) {
-  showBtn(toggleBtn);
-  toggleFavoriteIcon(isFavorite, toggleBtn);
+  showBtn(favoriteBtn);
+  toggleFavoriteBtnIcon(isFavorite, favoriteBtn);
 }
 
-function toggleFavoriteIcon(isFavorite, el) {
+function toggleFavoriteBtnIcon(isFavorite, el) {
   el.classList.toggle("fa", isFavorite);
   el.classList.toggle("far", !isFavorite);
 }
@@ -60,7 +59,7 @@ function hideFavoriteCard(text) {
 
 export {
   handleFavorite,
-  toggleFavoriteIcon,
+  toggleFavoriteBtnIcon as toggleFavoriteIcon,
   showFavoriteCard,
   showBtn,
   hideBtn,
