5dc1980f89b357d310e50856912b681b94d3fad4
Parvani Vafa <porvanivafoo@gmail.com>
2025-08-12 03:05:06 +0600

    Remove specific favorite quote

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,56 +1,67 @@
-import { favoriteBtn } from "../../index.js";
+import { quoteFavoriteBtn } from "../../index.js";
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
   toggleFavoriteBtnIcon(quote.isFavorite, btn);
+
   if (quote.isFavorite) {
     showFavoriteCard(quote, container);
   } else {
-    hideFavoriteCard(quote.id);
+    removeFavoriteCard(quote.id);
   }
 }
 
 function handleFavorite(isFavorite) {
-  showFavoriteBtn(favoriteBtn);
-  toggleFavoriteBtnIcon(isFavorite, favoriteBtn);
+  showFavoriteBtn();
+  toggleFavoriteBtnIcon(isFavorite);
 }
 
-function toggleFavoriteBtnIcon(isFavorite, el) {
-  el.classList.toggle("fa", isFavorite);
-  el.classList.toggle("far", !isFavorite);
+function toggleFavoriteBtnIcon(isFavorite) {
+  quoteFavoriteBtn.classList.toggle("fa", isFavorite);
+  quoteFavoriteBtn.classList.toggle("far", !isFavorite);
 }
 
-function showFavoriteBtn(btn) {
-  btn.style.display = "inline-block";
+function showFavoriteBtn() {
+  quoteFavoriteBtn.style.display = "inline-block";
 }
 
-function hideFavoriteBtn(btn) {
-  btn.style.display = "none";
+function hideFavoriteBtn() {
+  quoteFavoriteBtn.style.display = "none";
+}
+
+function removeFavoriteQuote(quote) {
+  quote.isFavorite = false;
+  removeFavoriteCard(quote.id);
+  const currentQuote = document.querySelector("[data-current-quote-id]");
+  const currentQuoteId = currentQuote.dataset.currentQuoteId;
+  if (quote.id === currentQuoteId) {
+    toggleFavoriteBtnIcon(quote.isFavorite);
+  }
 }
 
 function showFavoriteCard(quote, container) {
   const { id, text, author } = quote;
   const favoriteCard = document.createElement("div");
   favoriteCard.classList.add("favorite-card");
-  favoriteCard.dataset.quoteId = id;
+  favoriteCard.dataset.favoriteQuoteId = id;
   favoriteCard.innerHTML = `
+  <div class="favorite-card-content"
           <p>${text}</p>
-          <p class='author'>${author}</p>
+          <p class='favorite-card-author'>${author}</p>
+          </div>
+          <button class = "btn btn-danger"><i class="fas fa-trash-alt"></i>
+          Remove from favorites</button>
           `;
   container.appendChild(favoriteCard);
+  const removeButton = favoriteCard.querySelector(".btn-danger");
+  removeButton.addEventListener("click", () => removeFavoriteQuote(quote));
 }
 
-function hideFavoriteCard(id) {
-  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`);
+function removeFavoriteCard(id) {
+  const card = document.querySelector(`[data-favorite-quote-id="${id}"]`);
   if (card) {
     card.remove();
   }
-  // const favoriteCards = document.querySelectorAll(".favorite-card");
-  // favoriteCards.forEach((card) => {
-  //   if (card.textContent.includes(text)) {
-  //     card.remove();
-  //   }
-  // });
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
