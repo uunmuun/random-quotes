9e8bd794e06b98db45e17548d9211eb14776ae70
Parvani Vafa <porvanivafoo@gmail.com>
2025-08-12 21:49:28 +0600

    Remove specific favorite quote

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function setCurrentQuote(quote) {
 }
 
 const favoritesContainer = document.getElementById("favorites-container");
-const favoriteBtn = document.getElementById("favorite-btn");
-hideFavoriteBtn(favoriteBtn);
-favoriteBtn.addEventListener("click", () =>
-  toggleFavorite(currentQuote, favoriteBtn, favoritesContainer)
+const quoteFavoriteBtn = document.getElementById("quote-favorite-btn");
+hideFavoriteBtn();
+quoteFavoriteBtn.addEventListener("click", () =>
+  toggleFavorite(currentQuote, quoteFavoriteBtn, favoritesContainer)
 );
 const generateBtn = document.getElementById("generate-btn");
 generateBtn.addEventListener("click", () =>
   handleQuote(quotes, setCurrentQuote)
 );
 
-export { favoriteBtn };
+export { quoteFavoriteBtn };
